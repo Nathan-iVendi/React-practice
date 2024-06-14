b6e08ad1cdd7e13046676a2bc192c25e7a726daa
Nathan Yates <nathan.yates@ivendi.com>
2024-06-14 16:05:40 +0100

    Added Portal for confirmation of adoption

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,10 +1,13 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import ErrorBoundary from "./ErrorBoundary";
 import Carousel from "./Carousel";
 import fetchPet from "./fetchPet";
+import Modal from "./Modal";
 
 const Details = () => {
+  const [showModal, setShowModal] = useState(false); // Don't show by default
   const { id } = useParams();
   const results = useQuery(["details", id], fetchPet);
   // ["details", id] will be passed as the queryKey to fetchPet
@@ -33,8 +36,19 @@ const Details = () => {
         <h2>
           {pet.animal} - {pet.breed} - {pet.city} - {pet.state}
           <hr></hr>
-          <button>Adopt {pet.name}</button>
+          <button onClick={() => setShowModal(true)}>Adopt {pet.name}</button>
           <p>{pet.description}</p>
+          {showModal ? ( // If true, do this:
+            <Modal>
+              <div>
+                <h1>Would you like to adopt {pet.name}?</h1>
+                <div className="buttons">
+                  <button>Yes</button>
+                  <button onClick={() => setShowModal(false)}>No</button>
+                </div>
+              </div>
+            </Modal> // If false, render null/nothing
+          ) : null}
         </h2>
       </div>
     </div>
